512e5954174bb6942f26e5696eec0cc41b514fe3
heromen <15067179123@163.com>
2017-06-17 11:44:09 +0800

    fix: 库存管理的不对称

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -313,7 +313,6 @@ class Products extends Component {
               rowKey={record => record.id}
               pagination={paginationProps}
               rowSelection={rowSelection}
-              scroll={{ y: 500 }}
             />
           </Col>
         </Row>
